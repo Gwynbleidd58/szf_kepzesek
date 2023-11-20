c50dba44210a6d2c7e6575a5850a3b32d4752433
Gwynbleidd58 <obsitos.dominik@students.jedlik.eu>
2023-11-20 14:16:18 +0100

    Trainings kollekció létrehozása

--- routes/trainings.js
+++ routes/trainings.js
@@ -1,23 +1,28 @@
-const router = require('express').Router()
+const express = require('express')
+const router = express.Router()
 
-router.get('/api/trainings', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Show all trainings ' })
+router.get('/', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Összes képzés' })
 })
 
-router.get('/api/trainings/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Get training ${req.params.id}` })
+router.get('/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `A ${req.params.id} id-val rendelkező képzés` })
 })
 
-router.post('/api/trainings', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Create new training' })
+router.post('/', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Új képzés létrehozása' })
 })
 
-router.put('/api/trainings/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Update training ${req.params.id}` })
+router.put('/:id', (req, res) => {
+    res
+        .status(200)
+        .json({ success: true, msg: `Update training ${req.params.id}` })
 })
 
-router.delete('/api/trainings/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Delete training ${req.params.id}` })
+router.delete('/:id', (req, res) => {
+    res
+        .status(200)
+        .json({ success: true, msg: `Delete training ${req.params.id}` })
 })
 
 module.exports = router
