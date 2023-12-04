a8463f7bc497760ff8559da4f0e394e6915aae79
Gwynbleidd58 <obsitos.dominik@students.jedlik.eu>
2023-12-04 14:32:41 +0100

    Az alap vezérlők elkészültek

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ database.once("connected", () => {
 
 const app = express()
 
+app.use(express.json())
 app.use('/api/trainings', trainings)
 
-app.use(express.json())
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
